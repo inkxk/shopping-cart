e5fe0909b108f468f54a8298b074ce4b4355d455
Potcharapol Nokyoo <64063112+inkxk@users.noreply.github.com>
2022-10-24 16:19:06 +0700

    init navbar

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,21 @@ import { Container } from "react-bootstrap";
 import Home from "./pages/Home";
 import Store from "./pages/Store";
 import About from "./pages/About";
+import Navbar from "./components/Navbar";
 import "./App.css";
 
 function App() {
     return (
-        <Container>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/store" element={<Store />} />
-                <Route path="/about" element={<About />} />
-            </Routes>
-        </Container>
+        <>
+            <Navbar />
+            <Container className="mb-4">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/store" element={<Store />} />
+                    <Route path="/about" element={<About />} />
+                </Routes>
+            </Container>
+        </>
     );
 }
 
